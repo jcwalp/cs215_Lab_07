5e395ca96e287e4e983a0923f505492179811024
Jon <jwalp@sjci.com>
2019-11-13 14:05:24 -0500

    Confusion

--- cs215_Lab7_2019/cs215_lab07B/server.js
+++ cs215_Lab7_2019/cs215_lab07B/server.js
@@ -1,20 +1,44 @@
-var express = require("express"),
-  http = require("http"),
-  app = express();
 
-var PORT = 8080;
+
+
+var express = require('express');
+var http = require('http');
 var path = require('path');
+var app = express();
+var PORT = 8080;
 
-// set up a static file directory to use for default routing
-app.use(express.static(__dirname + "/www"));
 
-// Create our Express-powered HTTP server
+
+
+
+
 http.createServer(app).listen(PORT);
 
+
+
+
+// MIDDLEWARE
+
+app.use(express.static(path.join(__dirname + '/www')));
+
+
+
+// set up the root route for the site  and the response (res)
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/brightIdeas/index.html')
 });
+// set up the signUp route for the site  and the response (res)
+app.get("/signup", function(req, res){
+  res.sendFile(__dirname + '/brightIdeas/signup.html')
+});
+// set up the faq route for the site  and the response (res)
+
+
+//app.post method here for signUp
 
 
 
-console.log("Server running on port " + PORT);
+
+
+
+console.log("Server running on port "+ PORT);
